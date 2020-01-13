e347e411bb81977728cb8481ede3c0bf2e6eaf0d
akanksha <akanksha.dixit12@gmail.com>
2020-01-13 17:58:58 +0000

    added registerManager

--- IoTSmartContract.sol
+++ IoTSmartContract.sol
@@ -1,19 +1,24 @@
 pragma solidity ^0.5.11;
 
 contract IoTSmartContacrt{
+     struct Manager{
+        string name;
+        bytes32 managerID;
+        Device[] devices;
+    }
     struct Device{
         address manager;
         bytes32 deviceID;
         bytes32 firmwareHash;
     }
-    struct Manager{
-        string name;
-        bytes32 managerID;
-        Device[] devices;
+    mapping(address => Manager) public ManagerAddress;
+    mapping (address => uint) public ManagerDeviceCount;
+
+    function RegisterManager(string memory _name, bytes32 _id) public returns(address){
+        ManagerAddress[msg.sender].name = _name;
+        ManagerAddress[msg.sender].managerID = _id;
     }
-
-    mapping(address => Manager) public ManagerAdd;
     function RegisterDevice(bytes32 _identifier,address _manager, bytes32 firmwareHash) public returns(address){
-
+        
     }
 }
